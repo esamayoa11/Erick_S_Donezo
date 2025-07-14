92289f512b89a771ab460136f4e43b178140c9a0
Erick Samayoa <ericksamayoa11@gmail.com>
2025-07-14 14:42:45 -0700

    Organized and cleaned up server.js code to include some comments to identify each purpose.

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,14 @@ import todoRouter from "./routes/todo.js";
 const app = express();
 const PORT = 8080;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// todoRouter for the /todos
 app.use("/todos", todoRouter);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
 });
