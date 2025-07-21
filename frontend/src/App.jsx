66e037d1a467ae99f35d4ae997d92a08c0a613b4
Erick Samayoa <ericksamayoa11@gmail.com>
2025-07-21 16:10:16 -0700

    Completed Auth Layout (steps 36-38) and The Routes (steps 39-42)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,15 @@
-import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
-import { QueryClientProvider, QueryClient} from "@tanstack/react-query";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import MainLayout from "./layouts/main-layout";
 import ProtectedRoute from "./components/protected-route";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 import Todos from "./pages/todos";
+import { QueryClientProvider, QueryClient} from "@tanstack/react-query";
+
+
 import { useEffect } from "react";
 
 const client = new QueryClient();
-
-function RedirectToLogin() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    navigate("/login")
-  }, [])
-
-  return <></>
-}
 
 function App() {
   return (
@@ -26,10 +18,17 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/todos" element={<MainLayout />}>
+            <Route path="/todos" element={
+              <ProtectedRoute>
+                <Todos />
+              </ProtectedRoute>
+            } />
+          </Route>
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/layouts/main-layout.jsx
+++ frontend/src/layouts/main-layout.jsx
@@ -1,3 +1,21 @@
-export default function MainLayout(){
+// 37. import outlet from reacter-router-dom
+import { Link, Outlet } from 'react-router-dom';
 
+// 38. implement MainLayout component with navigation bar and outlet 
+export default function MainLayout(){
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">Donezo</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><button className="btn btn-link">Logout</button></li>
+          </ul>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  )
 }
